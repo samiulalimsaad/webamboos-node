87e771cbeb2b6aa2d5303a9c57f97b2a631464b6
samiulalimsaad <saadnbiuraj@gmail.com>
2022-06-07 12:41:14 +0600

    product add option added

--- index.ts
+++ index.ts
@@ -1,14 +1,38 @@
 import * as dotenv from "dotenv";
 import * as express from "express";
 import mongoose from "mongoose";
+import { Product } from "./Model/Product.model";
+import { productValidationSchema } from "./Validation/Product.validate";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.get("/", (req: express.Request, res: express.Response) => {
     res.json({ message: "hello" });
+});
+
+app.post("/product", async (req: express.Request, res: express.Response) => {
+    try {
+        const data = await productValidationSchema.validate(req.body, {
+            abortEarly: false,
+        });
+
+        data.status = "Inserted";
+
+        const newProduct = new Product(data);
+
+        const product = await newProduct.save();
+
+        res.status(200).json({ message: "Inserted", success: true, product });
+    } catch (error) {
+        res.json({
+            message: error.errors.length ? error.errors : error.message,
+        });
+    }
 });
 
 app.listen(PORT, () => {
