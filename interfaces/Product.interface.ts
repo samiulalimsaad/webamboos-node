1ce8b55b65a92efc48ea0a9db91b72f5c1dedad8
samiulalimsaad <saadnbiuraj@gmail.com>
2022-06-07 12:40:01 +0600

    product interface  added

---
+++ interfaces/Product.interface.ts
@@ -0,0 +1,10 @@
+// Create an interface representing a document in MongoDB.
+export interface ProductInterface {
+    price: number;
+    productName: string;
+    productQuantity: Number;
+    paymentMethod: string;
+    address: string;
+    email: string;
+    status: string;
+}
